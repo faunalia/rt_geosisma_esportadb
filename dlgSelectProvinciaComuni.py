c83f200b001a51a7dba130d3d9638993b7cbbda6
Luigi Pirelli <luipir@gmail.com>
2013-11-04 11:54:42 +0100

    compiled

--- dlgSelectProvinciaComuni.py
+++ dlgSelectProvinciaComuni.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'dlgSelectProvinciaComuni.ui'
 #
-# Created: Mon Nov  4 11:41:47 2013
+# Created: Mon Nov  4 11:54:03 2013
 #      by: PyQt4 UI code generator 4.9.3
 #
 # WARNING! All changes made in this file will be lost!
