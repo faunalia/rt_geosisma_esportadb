5e9b2bb840f9f8edd569c0d62d7f7eeebd2f7648
Luigi Pirelli <luipir@gmail.com>
2014-05-27 09:49:07 +0200

    added fab_10k_mod and sopralluoghi. Sopralluoghi is created here because instead in geosismadb because it has a different srid 23632 instead of 3003

--- schemas/geosisma_geo.sql
+++ schemas/geosisma_geo.sql
@@ -139,3 +139,46 @@ CREATE TABLE fab_10k
 );
 SELECT AddGeometryColumn( 'fab_10k', 'the_geom', 32632, 'MULTIPOLYGON', 'XY');
 SELECT CreateSpatialIndex('fab_10k', 'the_geom');
+
+------------------------------------------------------------------------
+
+CREATE TABLE fab_10k_mod
+(
+  "local_gid"   integer NOT NULL, -- chiave primaria nel db locale
+  "gid"         integer NOT NULL, -- chiave primaria del record nel db remoto
+  "identif"     text, -- aggregato identifier
+  "fab_10k_gid" integer, -- source fab_10k record fk
+  "mod_time"    date, -- client side modification datetime
+  "team_id"     text, -- team identifier as API URL
+  "upload_time" date, -- upload datetime
+  CONSTRAINT fab_10k_mod_pkey PRIMARY KEY (local_gid)
+);
+SELECT AddGeometryColumn( 'fab_10k_mod', 'the_geom', 32632, 'MULTIPOLYGON', 'XY');
+SELECT CreateSpatialIndex('fab_10k_mod', 'the_geom');
+
+------------------------------------------------------------------------
+
+CREATE TABLE "sopralluoghi" (
+  "gid" integer NOT NULL PRIMARY KEY,
+  "foglio" text,
+  "codice" varchar(4),
+  "record" float,
+  "topon" varchar(50),
+  "area" float,
+  "identif" varchar(10),
+  "id_hist" integer,
+  "time_start" datetime,
+  "time_end" datetime,
+  "id_scheda" integer,
+  "aggregato" text,
+  "edificio" integer,
+  "esito" varchar(1),
+  "id_evento" integer,
+  "controllo" boolean,
+  "data" date
+);
+SELECT AddGeometryColumn( 'sopralluoghi', 'the_geom', 32632, 'MULTIPOLYGON', 'XY');
+SELECT CreateSpatialIndex('sopralluoghi', 'the_geom');
+
+CREATE INDEX "sopralluoghi_401bf584" ON "sopralluoghi" ("gid");
+
